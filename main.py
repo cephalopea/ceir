719994654972ecb7f66d15b5182d8099e7692140
Caiti Hensley <caiti@eduroam127-22.hampshire.edu>
2019-03-19 13:29:07 -0400

    dkfj

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import pandas
-import numpy
+import pandas as pd
+import numpy as np
 
-pandas.read_csv("geert-hofstede.csv")
+ntlData = pd.read_csv("geert-hofstede.csv")
+ntlData = ntlData.replace('#NULL!', 'NaN')
+ntlData = ntlData.dropna()
+print(ntlData)
