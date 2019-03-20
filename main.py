d3470872df848f6e5e8415221dc3365a17d67a0d
Caiti Hensley <caiti@Kepler.local>
2019-03-20 15:38:37 -0400

    added distance functions and some convenience stuff

--- main.py
+++ main.py
@@ -2,5 +2,31 @@ import pandas as pd
 import numpy as np
 
 ntlData = pd.read_csv("geert-hofstede.csv")
-ntlData = ntlData.replace('#NULL!', 'NaN')
+ntlData.fillna(60, inplace=True) #center val as placeholder for missing vals
 
+sampleOutcome = {'pdi': 0, 'idv': 10, 'mas': 20, 'uai': 30, 'ltowvs': 40, 'ivr': 50}
+
+#next two functions are unnecessary, but convenient for clarity
+
+#creates a dataframe of just the relevant country's data
+def getCountryData(ctr):
+    return ntlData.loc[ntlData['ctr'] == ctr]
+
+def getDataPoint(row, attribute):
+    return row.iloc[0][attribute]
+
+#finds the average distance between a country's attributes and an outcome's attributes
+def getDistance(ctr, outcome):
+    ctrData = getCountryData(ctr) #dataframe of just the input country
+    ctrDistance = {}
+    attributes = ['pdi', 'idv', 'mas', 'uai', 'ltowvs', 'ivr']
+    dist = 0
+    for attribute in attributes:
+        ctrDistance[attribute] = abs(getDataPoint(ctrData, attribute) - outcome[attribute])
+    for attribute in attributes:
+        dist += ctrDistance[attribute]
+    dist /= 6
+    print(dist)
+    return dist
+
+getDistance('IND', sampleOutcome)
